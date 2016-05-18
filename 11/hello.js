4e0b027080074d64c73c1f0be571467890ca61ac
loya2016 <645179986@qq.com>
2016-05-18 10:40:29 +0800

    12

--- 11/hello.js
+++ 11/hello.js
@@ -1,1 +1,7 @@
 alert("你好漂亮")
+var student ={
+	name:"bbb",
+	age:21
+}
+console.log(student.name);
+console.log(student.age);
---
+++ 12/object.js
@@ -0,0 +1,12 @@
+
+var student ={
+	name:"bbb",
+	age:21,
+	sayhello:function(){
+		console.log("你好漂亮");
+	}
+	
+}
+console.log(student.name);
+console.log(student.age);
+student.sayhello();
